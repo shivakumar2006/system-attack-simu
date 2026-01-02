b2c38e4c05fd6092ffbc3b568d8308d32960a10f
shivakumar2006 <official.shivakumar06@gmail.com>
2026-01-02 19:42:37 +0530

    commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+
+const App = () => {
+    return (
+        <Routes>
+            <Route path="/" element={Home} />
+            <Route path="/signup" element={Signup} />
+            <Route path="/login" element={Login} />
+        </Routes>
+    )
+}
+
+export default App

6300dcc3db11296657b2c05507cbb69ce801bb91
shivakumar2006 <official.shivakumar06@gmail.com>
2026-01-02 15:40:59 +0530

    commit

--- backend/auth/controllers/auth.go
+++ backend/auth/controllers/auth.go
@@ -3,11 +3,13 @@ package controllers
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"time"
 
 	"auth/models"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/golang-jwt/jwt/v5"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
@@ -83,4 +85,180 @@ func Signup(w http.ResponseWriter, r *http.Request) {
 		"token": tokenString,
 		"user":  user,
 	})
+}
+
+func Login(w http.ResponseWriter, r *http.Request) {
+	var reqUser models.User
+
+	if err := json.NewDecoder(r.Body).Decode(&reqUser); err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var user models.User
+	err := UserCollection.FindOne(ctx, bson.M{"email": reqUser.Email}).Decode(&user)
+	if err != nil {
+		http.Error(w, "invalid email or password", http.StatusInternalServerError)
+		return
+	}
+
+	// compare password
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(reqUser.Password))
+	if err != nil {
+		http.Error(w, "email or password incorrect", http.StatusUnauthorized)
+		return
+	}
+
+	// generate token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"userId": user.ID.Hex(),
+		"exp":    time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := token.SignedString(JwtKey)
+	if err != nil {
+		http.Error(w, "failed to generate token", http.StatusInternalServerError)
+		return
+	}
+
+	user.Password = ""
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"token": tokenString,
+		"user":  user,
+	})
+}
+
+func Verify(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	tokenHeader := r.Header.Get("Authorization")
+	if tokenHeader == "" {
+		http.Error(w, "Invalid token", http.StatusUnauthorized)
+		return
+	}
+
+	// header is Bearere token
+	var tokenString string
+	fmt.Sscanf(tokenHeader, "Bearer %s", &tokenString)
+
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return JwtKey, nil
+	})
+	if err != nil || !token.Valid {
+		http.Error(w, "Invalid token", http.StatusUnauthorized)
+		return
+	}
+
+	// check if the token claims is valid or not
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		http.Error(w, "Invalid token claims", http.StatusUnauthorized)
+		return
+	}
+
+	userId := claims["userId"].(string)
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var user models.User
+	objId, _ := primitive.ObjectIDFromHex(userId)
+	err = UserCollection.FindOne(ctx, bson.M{"_id": objId}).Decode(&user)
+	if err != nil {
+		http.Error(w, "user not found", http.StatusNotFound)
+		return
+	}
+
+	user.Password = ""
+
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"valid": true,
+		"user":  user,
+	})
+}
+
+func ForgotPassword(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Email string `json:"email"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	var user models.User
+	err := UserCollection.FindOne(ctx, bson.M{"email": req.Email}).Decode(&user)
+	if err != nil {
+		http.Error(w, "Email not found", http.StatusNotFound)
+		return
+	}
+
+	// genetate a token only for 15 minutes
+	resetToken := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"email": user.Email,
+		"exp":   time.Now().Add(time.Hour * 24).Unix(),
+	})
+
+	tokenString, err := resetToken.SignedString(JwtKey)
+	if err != nil {
+		http.Error(w, "failed to generate token", http.StatusInternalServerError)
+		return
+	}
+
+	resetLink := fmt.Sprintf("http://localhost:5173/reset-password?token=%s", tokenString)
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{
+		"message":    "password reset link is generated",
+		"reset_link": resetLink,
+	})
+}
+
+func ResetPassword(w http.ResponseWriter, r *http.Request) {
+	var req struct {
+		Token    string `json:"token"`
+		Password string `json:"password"`
+	}
+
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid input", http.StatusBadRequest)
+		return
+	}
+
+	// parse and verify jwt
+	token, err := jwt.Parse(req.Token, func(token *jwt.Token) (interface{}, error) {
+		return JwtKey, nil
+	})
+	if err != nil || !token.Valid {
+		http.Error(w, "Invalid and expires a token", http.StatusUnauthorized)
+		return
+	}
+
+	claims, ok := jwt.Claims.(jwt.MapClaims)
+	if !ok {
+		http.Error(w, "Invalid jwt token claims", http.StatusInternalServerError)
+		return
+	}
+
+	email := claims["email"].(string)
+
+	// hash our password
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(req.Password), bcrypt.DefaultCost)
+	if err != nil {
+		http.Error(w, "Error hashing password", http.StatusInternalServerError)
+		return
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
 }
